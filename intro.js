7d016e3d5490695665554bbb2e3b858930ea3874
Pritpal Singh <psingh50@mail.greenriver.edu>
2017-05-17 01:06:01 -0500

    Wrote the countBs function as step 3

--- intro.js
+++ intro.js
@@ -1,6 +1,7 @@
 console.log("Introduction to JavaScript");
 
 //Fizz Buzz
+console.log("*********************FizzBuzz**************************");
 for (var counter = 1; counter <= 100; counter++) {
 	if (counter % 3 === 0) {
 		console.log("Fizz");
@@ -12,3 +13,17 @@ for (var counter = 1; counter <= 100; counter++) {
 		console.log(counter);
 	}
 }
+
+//Bean Counting
+function countBs(inputStr) {
+	var count = 0;
+	for(var i = 0; i < inputStr.length; i++) {
+		if (inputStr.charAt(i) == 'B') {
+			count++;
+		}
+	}
+	return count;
+}
+console.log("*********************Bean Counting**************************");
+console.log(countBs("BBC"));
+console.log(countChar("kakkerlak","k"));
