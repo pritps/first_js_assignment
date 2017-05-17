ac8fb72755b215f50e9735e74ce4615cb49d5ca0
Pritpal Singh <psingh50@mail.greenriver.edu>
2017-05-17 00:38:04 -0500

    Wrote the FizzBuzz program as step 2

--- intro.js
+++ intro.js
@@ -1,1 +1,14 @@
 console.log("Introduction to JavaScript");
+
+//Fizz Buzz
+for (var counter = 1; counter <= 100; counter++) {
+	if (counter % 3 === 0) {
+		console.log("Fizz");
+	} else if (counter % 5 === 0 && counter % 3 !== 0) {
+		console.log("Buzz");
+	} else if (counter % 15 ===0) {
+		console.log("FizzBuzz");
+	} else {
+		console.log(counter);
+	}
+}
